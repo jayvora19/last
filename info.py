2e4c92fc39b7be71da6b7bdef4edef2ff5c9928b
jayvora19 <75425454+jayvora19@users.noreply.github.com>
2023-05-17 11:16:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = "26861990"
+API_HASH     = "0592761ae3a24dcf709d85ab87bc12b9"
+BOT_TOKEN    = "6004178116:AAGXlZx-VWry9DVw2hooFQrwmKkB8tL1uuc"
+SESSION      = "BQGZ4aYAvtsIfMJpRIIBLTKskWOD9-zdV25gexQ-744mAMfM9xDKmtD4LsGdWp4srQtcl-U-n6v241JtTxORuffwdE_E3flodf4ZXt_qI5ELZ8w1upGaYlvnjIjw8De_Vj_idleWlhdXj7o2oTYEuCkU49wx7S_w3GfnVo9Hx-VzjRMKp2zjkErdNa5UsXxKt-xuSsCF6iRcMXvT08jMOYIiidJ3snEX4ZbUYtojEzlzajmnaB8DOkq1waL8DwVOGp6RQvizJOrFtxi_FPbmxtvEoCClKHfHvEP_tsn6B--dtS3V26vwC5XA_pT0TkeCbCRJ4t6Q4_u5VHPYamd7Q90g7qAr1AAAAAFnifBOAA"
+DATABASE_URI = "mongodb+srv://jayvora:1850@cluster.t5ql7o3.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = "1001847058383"
+ADMIN        = "6032060494"
+CHANNEL      = "@FreeFlix18"
